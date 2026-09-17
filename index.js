573a6bac013c733e6a620afa755914456327ffa8
mooka00 <maksimalka5@list.ru>
2026-09-17 17:48:22 +0500

    инициализация приложения

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+app.get('/dotaorlol', (req, res) => {
+  res.send('dota 2');
+});
+app.get('/bestdota2hero', (req, res) => {
+  res.send('phantom lanser');
+});
+app.get('/worstdota2hero', (req, res) => {
+  res.send('axe');
+});
+app.get('/whatdota2playerslove', (req, res) => {
+  res.send('man');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
